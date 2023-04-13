55a8a4ba9238856be073944ffc7d654b457e07d0
BrenoFariasdaSilva <breno.vicio78@gmail.com>
2023-04-13 15:27:00 -0300

    Adding things inside HelloWorld

--- OneBitCode/01-Introduction/HelloWorld.rb
+++ OneBitCode/01-Introduction/HelloWorld.rb
@@ -2,3 +2,6 @@ puts "Hello World without parentheses!"
 puts ("Hello World with parentheses!\n")
 puts ("Hello World with quotes! \nIn quotes the text can have special characters like the new line character \\n")
 puts ('Hello World with apostrophe cant include special characters. It will be interpreted as literal, so the \n aint working!')
+
+puts ("Printing puts (12*12): ")
+puts (12*12)
