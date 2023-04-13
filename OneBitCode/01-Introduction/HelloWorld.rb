7aa1ab73a1dcf7a8cff89c6b4649191e6c40ba28
BrenoFariasdaSilva <breno.vicio78@gmail.com>
2023-04-13 15:23:50 -0300

    HelloWorld

---
+++ OneBitCode/01-Introduction/HelloWorld.rb
@@ -0,0 +1,4 @@
+puts "Hello World without parentheses!"
+puts ("Hello World with parentheses!\n")
+puts ("Hello World with quotes! \nIn quotes the text can have special characters like the new line character \\n")
+puts ('Hello World with apostrophe cant include special characters. It will be interpreted as literal, so the \n aint working!')
