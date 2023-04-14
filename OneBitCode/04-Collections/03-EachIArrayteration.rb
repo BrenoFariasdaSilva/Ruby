8282d1a06bbe61d053a36f4a12ca46f5b7dfcb58
BrenoFariasdaSilva <breno.vicio78@gmail.com>
2023-04-13 22:35:00 -0300

    Each vs Map vs Select

--- OneBitCode/04-Collections/03-EachIArrayteration.rb
+++ OneBitCode/04-Collections/03-EachIArrayteration.rb
@@ -1,5 +1,10 @@
 # Program to show the usage of the each method in collections - Array
 
+# Each vs Map vs Select
+# Each: Iterates over the elements of the collection without modifying the variable beetwen pipes
+# Map: Iterates over the elements of the collection and returns a new collection with the results of the block
+# Select: Iterates over the elements of the collection and returns a new collection with the elements that match the condition of the block
+
 names = ["Manoel", "Marcia", "Amanda", "Batistela", "Pamella", "Thaynara"]
 
 name = "Breno Farias"
