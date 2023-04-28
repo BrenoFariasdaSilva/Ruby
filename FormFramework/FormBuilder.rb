9b12d5d329530b1c799d4743ae52c3528c224e3a
BrenoFariasdaSilva <breno.vicio78@gmail.com>
2023-04-28 20:18:42 -0300

    Improving FormBuilder code

--- FormFramework/FormBuilder.rb
+++ FormFramework/FormBuilder.rb
@@ -1,107 +1,169 @@
 # Import the MySQL client library
-require 'mysql2'
+# require 'mysql2'
 
 class FormBuilder
    # Define getter and setter methods for form_fields and form_data
-   attr_accessor :form_fields, :form_data
+   # attr_accessor :form_fields, :form_data
 
    # Constructor that takes the action, method and database config as arguments
-   def initialize(action, method, db_config)
-      # Store the action and method as instance variables
-      @action = action
-      @method = method
-      # Create an empty array to hold the form fields
-      @form_fields = []
-      # Create a new MySQL client object using the provided config
-      @db = Mysql2::Client.new(db_config)
-      # Create an empty hash to hold the form data
-      @form_data = {}
-   end
+   # def initialize()
+   #    # Store the action and method as instance variables
+   #    # @action = action
+   #    # @method = method
+   #    # Create an empty string to hold the HTML form
+   #    @form_fields = []
+   #    # Create a new MySQL client object using the provided config
+   #    # @db = Mysql2::Client.new(db_config)
+   #    # Create an empty hash to hold the form data
+   #    @form_data = {}
+   # end
 
    # Method for adding a form field to the form_fields array
    def add_field(type, name, label, options = {})
       # Create a new hash to hold the field's information
-      field = {type: type, name: name, label: label, options: options}
+      # field = {type: type, name: name, label: label, options: options}
       # Add the field hash to the form_fields array
-      @form_fields << field
+      # @form_fields << field
    end
 
    # Method for building the HTML form
-   def build_form
+   def build_form(type, name, label, options = {})
+      form_html = "<!DOCTYPE html>
+      <html>
+        <head>
+          <title>HTML Form</title>
+        </head>
+        <body>"
       # Start the HTML form tag with the action and method attributes
-      form_html = "<form action=\"#{@action}\" method=\"#{@method}\">"
+      # form_html = "<form action=\"#{action}\" method=\"#{method}\">"
+      # form_html += "<br>"
+      # form_html += "\n"
+
       # Iterate over each form field in the form_fields array
-      @form_fields.each do |field|
-         # Use a case statement to handle each type of form field
-      case field[:type]
-         when "text"
-            # Add a label for the text input
-            form_html += "<label for=\"#{field[:name]}\">#{field[:label]}</label>"
-            # Add the text input with the name and id attributes
-            form_html += "<input type=\"#{field[:type]}\" name=\"#{field[:name]}\" id=\"#{field[:name]}\"#{required_field(field[:options])}>"
-         when "textarea"
-            # Add a label for the textarea
-            form_html += "<label for=\"#{field[:name]}\">#{field[:label]}</label>"
-            # Add the textarea with the name and id attributes
-            form_html += "<textarea name=\"#{field[:name]}\" id=\"#{field[:name]}\"#{required_field(field[:options])}></textarea>"
-         when "checkbox"
-            # Add a label for the checkbox group
-            form_html += "<label>#{field[:label]}</label>"
-            # Iterate over each checkbox option
-            field[:options].each do |option|
-               # Add the checkbox with the name, value and id attributes
-               form_html += "<input type=\"#{field[:type]}\" name=\"#{field[:name]}[]\" value=\"#{option[:value]}\"#{required_field(field[:options])}>#{option[:label]}"
+         case type
+            when "text"
+               # Add a label for the text input
+               form_html = form_html + "<label for=\"#{name}\">#{label}</label>"
+
+               form_html += "\n"
+               # Add the text input with the name and id attributes
+               form_html = form_html + "<input type=\"#{type}\" name=\"#{name}\" id=\"#{name}\">"
+            when "textarea"
+               # Add a label for the text input
+               form_html = form_html + "<label for=\"#{name}\">#{label}</label>"
+               form_html += "\n"
+               # Add the text input with the name and id attributes
+               form_html = form_html + "<textarea name=\"#{name}\" id=\"#{name}\"></textarea>"
+            when "select"
+               # Add a label for the text input
+               form_html = form_html + "<label for=\"#{name}\">#{label}</label>"
+               form_html += "\n"
+               # Add the text input with the name and id attributes
+               form_html = form_html + "<select name=\"#{name}\" id=\"#{name}\">"
+               form_html += "\n"
+               # Add the text input with the name and id attributes
+               options.each do |option|
+                  form_html = form_html + "<option value=\"#{option}\">#{option}</option>"
+                  form_html += "\n"
+               end
+               form_html = form_html + "</select>"
+            when "radio"
+               # Add a label for the text input
+               form_html = form_html + "<label for=\"#{name}\">#{label}</label>"
+               form_html += "\n"
+               # Add the text input with the name and id attributes
+               options.each do |option|
+                  form_html = form_html + "<input type=\"#{type}\" name=\"#{name}\" id=\"#{name}\" value=\"#{option}\">"
+                  form_html = form_html + "<label for=\"#{name}\">#{option}</label>"
+                  form_html += "\n"
+               end
+            when "checkbox"
+               # Add a label for the text input
+               form_html = form_html + "<label for=\"#{name}\">#{label}</label>"
+               form_html += "\n"
+               # Add the text input with the name and id attributes
+               options.each do |option|
+                  form_html = form_html + "<input type=\"#{type}\" name=\"#{name}\" id=\"#{name}\" value=\"#{option}\">"
+                  form_html = form_html + "<label for=\"#{name}\">#{option}</label>"
+                  form_html += "\n"
+               end
+            when "button"
+               # Add a label for the text input
+               form_html = form_html + "<label for=\"#{name}\">#{label}</label>"
+               form_html += "\n"
+               # Add the text input with the name and id attributes
+               form_html = form_html + "<input type=\"#{type}\" name=\"#{name}\" id=\"#{name}\" value=\"#{name}\">"
+               form_html += "\n"
+            else
+               # Add a label for the text input
+               form_html = form_html + "<label for=\"#{name}\">#{label}</label>"
+               form_html += "\n"
+               # Add the text input with the name and id attributes
+               form_html = form_html + "<input type=\"#{type}\" name=\"#{name}\" id=\"#{name}\">"
+            form_html += "<br>"
+            form_html += "\n"
             end
-         when "radio"
-            # Add a label for the radio button group
-            form_html += "<label>#{field[:label]}</label>"
-            # Iterate over each radio button option
-            field[:options].each do |option|
-               # Add the radio button with the name, value and id attributes
-               form_html += "<input type=\"#{field[:type]}\" name=\"#{field[:name]}\" value=\"#{option[:value]}\"#{required_field(field[:options])}>#{option[:label]}"
-            end
-         when "select"
-            # Add a label for the select menu
-            form_html += "<label for=\"#{field[:name]}\">#{field[:label]}</label>"
-            # Add the select
-            form_html += "<select name=\"#{field[:name]}\" id=\"#{field[:name]}\"#{required_field(field[:options])}>"
-            field[:options].each do |option|
-               form_html += "<option value=\"#{option[:value]}\">#{option[:label]}</option>"
-            end
-            form_html += "</select>"
-         end
-      end
+      # Add the submit button to the form
       form_html += "<input type=\"submit\" value=\"Submit\">"
+      # Add a line break after the submit button
+      form_html += "<br>"
+      # break line \n
+      form_html += "\n"
+
+      # Close the form tag
       form_html += "</form>"
+      form_html += "<br>"
+      form_html += "\n"
+      # Return the completed form HTML
+      form_html += "  </body>
+      </html>"
       return form_html
-      end
    end
 
-   def process_form(params)
-      @form_fields.each do |field|
-      if params.key?(field[:name])
-         if field[:type] == "checkbox"
-            @form_data[field[:name]] = params[field[:name]].join(",")
-         else
-            @form_data[field[:name]] = params[field[:name]]
-         end
-      end
-      insert_data
-   end
+   # Method for building the HTML table
+   # def build_table
+   #    # Start the HTML table tag
+   #    table_html = "<table>"
+   #    # Iterate over each key-value pair in the form_data hash
+   #    @form_data.each do |key, value|
+   #       # Add a table row with the key and value
+   #       table_html += "<tr><td>#{key}</td><td>#{value}</td></tr>"
+   #    end
+   #    # Close the table tag
+   #    table_html += "</table>"
+   #    # Return the completed table HTML
+   #    return table_html
+   # end
 
-   def insert_data
-      fields = @form_data.keys.join(",")
-      values = @form_data.values.map { |value| "'#{value}'" }.join(",")
-      sql = "INSERT INTO form_data (#{fields}) VALUES (#{values})"
-      @db.query(sql)
-   end
+   # # Method for saving the form data to the database
+   # def save_data
+   #    # Iterate over each key-value pair in the form_data hash
+   #    @form_data.each do |key, value|
+   #       # Insert the key and value into the database
+   #       @db.query("INSERT INTO form_data (name, value) VALUES ('#{key}', '#{value}')")
+   #    end
+   # end
 
-   def required_field(options)
-      if options.key?(:required)
-         if options[:required]
-            return " required"
-         end
-      end
-      return ""
-   end
+   # # Method for loading the form data from the database
+   # def load_data
+   #    # Query the database for all of the form data
+   #    results = @db.query("SELECT * FROM form_data")
+   #    # Iterate over each row in the results
+   #    results.each do |row|
+   #       # Add the row's name and value to the form_data hash
+   #       @form_data[row["name"]] = row["value"]
+   #    end
+   # end
+
+   # Method for checking if a field is required
+   # def required_field(options)
+   #    # Check if the required option is set to true
+   #    if options[:required]
+   #       # Return the required attribute
+   #       return " required"
+   #    else
+   #       # Return an empty string
+   #       return ""
+   #    end
+   # end
 end
--- FormFramework/main.rb
+++ FormFramework/main.rb
@@ -1,24 +1,41 @@
 require_relative "FormBuilder"
 
-print("Enter the type of the input: ")
-type = gets.chomp
-print("Enter the name of the input: ")
-name = gets.chomp
-print("Enter the label of the input: ")
-label = gets.chomp
-print("Enter the options of the input: ")
-options = gets.chomp
+# print("Enter the type of the input: ")
+# type = gets.chomp
+# print("Enter the name of the input: ")
+# name = gets.chomp
+# print("Enter the label of the input: ")
+# label = gets.chomp
+# print("Enter the options of the input: ")
+# options = gets.chomp
 
-@form_fields = [
-	{
-		type: type,
-		name: name,
-		label: label,
-		options: options
-	}
-]
 
-@form_data = {}
 
-html_string = FormBuilder.new(@form_fields, @form_data).build_form
-puts html_string
+# @form_fields = [
+	# 	{
+		# 		type: type,
+		# 		name: name,
+		# 		label: label,
+		# 		options: options
+		# 	}
+		# ]
+		
+		@form_data = {}
+		
+		# create a new instance of the FormBuilder class
+types_of_inputs = ["text", "textarea", "select", "radio", "checkbox", "button"]
+form = FormBuilder.new()
+
+html_form = ""
+
+# call the build_form method on the form instance
+for type in types_of_inputs
+	html_form = html_form + form.build_form(type, "name", "label", ["option1", "option2"])
+end
+
+# print the HTML string to the terminal
+puts html_form
+
+# now write this HTML string to a file named form.html
+File.write("form.html", html_form)
+
