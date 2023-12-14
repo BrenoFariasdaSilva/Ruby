439be8c12244763a1ac5ff733063612f602e72a0
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-12-14 17:54:24 -0300

    REFACTOR: Improving the Text File Content Sorter Ruby Algorithm Structure

--- Text File Content Sorter/main.rb
+++ Text File Content Sorter/main.rb
@@ -22,47 +22,59 @@ end
 
 # This method returns true if the URLs are sorted alphabetically, otherwise false
 def sorted_alphabetically?(urls)
-   names = urls.map { |url| get_name_from_url(url) }
-   names == names.sort
+   names = urls.map { |url| get_name_from_url(url) } # Get the names from the URLs
+   names == names.sort # Return true if the names are sorted alphabetically, otherwise false
 end
 
- # This method sorts the URLs alphabetically
+# This method sorts the URLs alphabetically
 def sort_urls_alphabetically(urls)
-   urls.sort_by { |url| get_name_from_url(url) }
+   urls.sort_by { |url| get_name_from_url(url) } # Sort the URLs alphabetically by the name
 end
 
 # This method returns the string after the last slash (not included) in the URL
 def get_name_from_url(url)
    # Extract the string after the last slash (not included) in the URL
    url.split("/").last
-   # replace all non-alphanumeric characters with a space
-   .gsub(/[^0-9A-Za-z]/, " ")
+      # replace all non-alphanumeric characters with a space
+      .gsub(/[^0-9A-Za-z]/, " ")
 end
 
-# Read URLs from the text file
-url_file_path = "../Magnet URLs/URLs.txt"
-urls = File.readlines(url_file_path, chomp: true)
-
-unless sorted_alphabetically?(urls)
-   puts "Sorting Text File Content Alphabetically..."
-   urls = sort_urls_alphabetically(urls)
-   File.write(url_file_path, urls.join("\n"))
-end
-
-puts "Text File Content Sorted Alphabetically"
-
-# Sound file path
-sound_file_path = "../.assets/Sounds/NotificationSound.wav"
-
-# Play sound at the end
-if File.exist?(sound_file_path)
-   if RbConfig::CONFIG["host_os"] =~ /linux/
-      # Linux command to play sound
-      system("aplay #{sound_file_path}")
-   elsif RbConfig::CONFIG["host_os"] =~ /mswin|msys|mingw|cygwin|bccwin|wince|emc/
-      # Windows command to play sound
-      system("start #{sound_file_path}")
+# This method plays a sound file based on the operating system
+def play_sound(sound_file_path)
+   if File.exist?(sound_file_path)
+      if RbConfig::CONFIG["host_os"] =~ /linux/
+         # Linux command to play sound
+         system("aplay #{sound_file_path}")
+      elsif RbConfig::CONFIG["host_os"] =~ /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+         # Windows command to play sound
+         system("start #{sound_file_path}")
+      end
+   else
+      puts "Sound file not found: #{sound_file_path}"
    end
-else
-   puts "Sound file not found: #{sound_file_path}"
 end
+
+# Main function
+def main
+   # Read URLs from the text file
+   url_file_path = "../Magnet URLs/URLs.txt"
+   urls = File.readlines(url_file_path, chomp: true)
+
+   # Sort the URLs alphabetically if they are not sorted alphabetically
+   unless sorted_alphabetically?(urls)
+      puts "Sorting Text File Content Alphabetically..."
+      urls = sort_urls_alphabetically(urls)
+      File.write(url_file_path, urls.join("\n"))
+   end
+
+   puts "Text File Content Sorted Alphabetically"
+
+   # Sound file path
+   sound_file_path = "../.assets/Sounds/NotificationSound.wav"
+
+   # Play sound at the end
+   play_sound(sound_file_path)
+end
+
+# Call the main function
+main
