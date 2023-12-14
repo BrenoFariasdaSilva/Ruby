1b5fda2880b05b533ba55f501415922cfa27ad6b
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-12-14 17:27:41 -0300

    REFACTOR: Improving the Matched Occurrences in CSV Structure Ruby Algorithm

--- Matched Occurrences in CSV/main.rb
+++ Matched Occurrences in CSV/main.rb
@@ -21,55 +21,65 @@ def get_geckodriver_path
    end
 end
 
-# Constants:
-SEARCH_COLUMN = "Status" # Specify the column to match the SEARCHED_OCCURRENCE value
-SEARCHED_OCCURRENCE = "Downloading" # Specify the value to match in the SEARCH_COLUMN
-VALUES_COLUMN = "Magnet URL" # Specify the column to get values from when the SEARCHED_OCCURRENCE is found in the SEARCH_COLUMN
-INPUT_CSV_FILEPATH = "../Magnet URLs/Magnet_URLs.csv"
-
 # This method returns an array of the occurrences from the VALUES_COLUMN when the SEARCHED_OCCURRENCE is found in the SEARCH_COLUMN
-def get_occurrences(INPUT_CSV_FILEPATH)
+def get_occurrences(input_csv_filepath, search_column, searched_occurrence, values_column)
    occurrences = [] # Initialize an empty array to store the occurrences
 
    # Read CSV file
-   CSV.foreach(INPUT_CSV_FILEPATH, headers: true) do |row|
-      search_column = row[SEARCH_COLUMN] # Get the value of the SEARCH_COLUMN
-      occurrence = row[VALUES_COLUMN] # Get the value of the VALUES_COLUMN
+   CSV.foreach(input_csv_filepath, headers: true) do |row|
+      search_column_value = row[search_column] # Get the value of the SEARCH_COLUMN
+      current_occurrence = row[values_column] # Get the value of the VALUES_COLUMN
 
-      # Verify if the SEARCHED_OCCURRENCE is found in the SEARCH_COLUMN
-      if search_column && occurrence && search_column == occurrence
-         occurrences << occurrence # Add the occurrence to the array
+      # Verify if the search_column_value is equal to the searched_occurrence
+      if search_column_value && search_column_value == searched_occurrence && current_occurrence
+         occurrences << current_occurrence # Add the current_occurrence to the array
       end
    end
 
    occurrences # Return the array of the found occurrences
 end
+
+# This method plays a sound file based on the operating system
+def play_sound(sound_file_path)
+   if File.exist?(sound_file_path)
+      if RbConfig::CONFIG["host_os"] =~ /linux/
+         # Linux command to play sound
+         system("aplay #{sound_file_path}")
+      elsif RbConfig::CONFIG["host_os"] =~ /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+         # Windows command to play sound
+         system("start #{sound_file_path}")
+      end
+   else
+      puts "Sound file not found: #{sound_file_path}"
+   end
+end
+
+# Main function
+def main
+   search_column = "Status" # Specify the column to match the searched_occurrence value
+   searched_occurrence = "Downloading" # Specify the value to match in the search_column
+   values_column = "Magnet URL" # Specify the column to get values from when the searched_occurrence is found in the search_column
+   input_csv_filepath = "../Magnet URLs/Magnet_URLs.csv"
+
+   # Get the occurrences from the values_column when the searched_occurrence is found in the search_column
+   occurrences = get_occurrences(input_csv_filepath, search_column, searched_occurrence, values_column)
+
+   # Save the found occurrences to a text file
+   output_file_path = "../Magnet URLs/#{search_column}:#{searched_occurrence}.txt"
+   File.open(output_file_path, "w") do |file|
+      occurrences.each do |magnet_url|
+         file.puts magnet_url
+      end
+   end
+
+   puts "Saving the values from the column '#{values_column}' to the file '#{output_file_path}' when the value '#{searched_occurrence}' is found in the '#{search_column}' column"
+
+   # Sound file path
+   sound_file_path = "../.assets/Sounds/NotificationSound.wav"
+
+   # Play sound at the end
+   play_sound(sound_file_path)
+end
  
-# Get the occurrences from the VALUES_COLUMN when the SEARCHED_OCCURRENCE is found in the SEARCH_COLUMN
-occurrences = get_occurrences(INPUT_CSV_FILEPATH)
-
-# Save the found occurrences to a text file
-output_file_path = "../Magnet URLs/#{SEARCH_COLUMN}:#{SEARCHED_OCCURRENCE}.txt"
-File.open(output_file_path, "w") do |file|
-   occurrences.each do |magnet_url|
-      file.puts magnet_url
-   end
-end
-
-puts "Saving the values from the column '#{VALUES_COLUMN}' to the file '#{output_file_path}' when the value '#{SEARCHED_OCCURRENCE}' is found in the column '#{SEARCH_COLUMN}'"
-
-# Sound file path
-sound_file_path = "../.assets/Sounds/NotificationSound.wav"
-
-# Play sound at the end
-if File.exist?(sound_file_path)
-   if RbConfig::CONFIG["host_os"] =~ /linux/
-      # Linux command to play sound
-      system("aplay #{sound_file_path}")
-   elsif RbConfig::CONFIG["host_os"] =~ /mswin|msys|mingw|cygwin|bccwin|wince|emc/
-      # Windows command to play sound
-      system("start #{sound_file_path}")
-   end
-else
-   puts "Sound file not found: #{sound_file_path}"
-end
+# Call the main function
+main
