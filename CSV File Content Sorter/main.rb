ea26603a68a9f6745a114babb75d47d932538dd3
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-12-14 17:37:01 -0300

    REFACTOR: Improving the CSV File Content Sorter Ruby Algorithm Structure

--- CSV File Content Sorter/main.rb
+++ CSV File Content Sorter/main.rb
@@ -1,8 +1,5 @@
-require "csv"
+require "csv" # Import the CSV gem
 require "rbconfig" # Import the RbConfig gem
-
-# Constant for sorting column
-SORTING_COLUMN = "Name"
 
 # This method returns the path to the geckodriver executable
 def get_geckodriver_path
@@ -24,32 +21,50 @@ def get_geckodriver_path
    end
 end
 
-# Read CSV file
-csv_file_path = "../Magnet URLs/Magnet_URLs.csv"
-csv_data = CSV.read(csv_file_path, headers: true)
+# This method sorts the CSV file content alphabetically by the specified column
+def sort_csv_alphabetically(csv_file_path, sorting_column)
+   csv_data = CSV.read(csv_file_path, headers: true)
 
-# Convert the CSV::Table to an array of hashes and sort
-csv_array = csv_data.sort_by { |row| row[SORTING_COLUMN] }
+   # Convert the CSV::Table to an array of hashes and sort
+   csv_array = csv_data.sort_by { |row| row[sorting_column] }
 
-# Write the sorted array back to the CSV file
-CSV.open(csv_file_path, "w", headers: csv_data.headers, write_headers: true) do |csv|
-   csv_array.each { |row| csv << row }
-end
-
-puts "CSV File Content Sorted Alphabetically by the #{SORTING_COLUMN} Column"
-
-# Sound file path
-sound_file_path = "../.assets/Sounds/NotificationSound.wav"
-
-# Play sound at the end
-if File.exist?(sound_file_path)
-   if RbConfig::CONFIG["host_os"] =~ /linux/
-      # Linux command to play sound
-      system("aplay #{sound_file_path}")
-   elsif RbConfig::CONFIG["host_os"] =~ /mswin|msys|mingw|cygwin|bccwin|wince|emc/
-      # Windows command to play sound
-      system("start #{sound_file_path}")
+   # Write the sorted array back to the CSV file
+   CSV.open(csv_file_path, "w", headers: csv_data.headers, write_headers: true) do |csv|
+      csv_array.each { |row| csv << row }
    end
-else
-   puts "Sound file not found: #{sound_file_path}"
+
+   puts "CSV File Content Sorted Alphabetically by the #{sorting_column} Column"
 end
+ 
+ # This method plays a sound file based on the operating system
+ def play_sound(sound_file_path)
+   if File.exist?(sound_file_path)
+      if RbConfig::CONFIG["host_os"] =~ /linux/
+         # Linux command to play sound
+         system("aplay #{sound_file_path}")
+      elsif RbConfig::CONFIG["host_os"] =~ /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+         # Windows command to play sound
+         system("start #{sound_file_path}")
+      end
+   else
+      puts "Sound file not found: #{sound_file_path}"
+   end
+end
+ 
+# Main function
+def main
+   # Specify the path to the CSV file
+   csv_file_path = "../Magnet URLs/Magnet_URLs.csv"
+
+   # Sort the CSV file alphabetically by the specified column
+   sort_csv_alphabetically(csv_file_path, SORTING_COLUMN)
+
+   # Sound file path
+   sound_file_path = "../.assets/Sounds/NotificationSound.wav"
+
+   # Play sound at the end
+   play_sound(sound_file_path)
+end
+
+# Call the main function
+main
