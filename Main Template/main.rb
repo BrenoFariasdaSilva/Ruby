d4499dc77b184ffe41084e6cce789cbc1afa35a6
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-12-14 17:30:51 -0300

    REFACTOR: Improving the Main Template Ruby Algorithm Structure

--- Main Template/main.rb
+++ Main Template/main.rb
@@ -20,18 +20,29 @@ def get_geckodriver_path
    end
 end
 
-# Sound file path
-sound_file_path = "../.assets/Sounds/NotificationSound.wav"
-
-# Play sound at the end
-if File.exist?(sound_file_path)
-   if RbConfig::CONFIG["host_os"] =~ /linux/
-      # Linux command to play sound
-      system("aplay #{sound_file_path}")
-   elsif RbConfig::CONFIG["host_os"] =~ /mswin|msys|mingw|cygwin|bccwin|wince|emc/
-      # Windows command to play sound
-      system("start #{sound_file_path}")
+# This method plays a sound file based on the operating system
+def play_sound(sound_file_path)
+   if File.exist?(sound_file_path)
+      if RbConfig::CONFIG["host_os"] =~ /linux/
+         # Linux command to play sound
+         system("aplay #{sound_file_path}")
+      elsif RbConfig::CONFIG["host_os"] =~ /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+         # Windows command to play sound
+         system("start #{sound_file_path}")
+      end
+   else
+      puts "Sound file not found: #{sound_file_path}"
    end
-else
-   puts "Sound file not found: #{sound_file_path}"
 end
+
+# Main function
+def main
+   # Sound file path
+   sound_file_path = "../.assets/Sounds/NotificationSound.wav"
+
+   # Play sound at the end
+   play_sound(sound_file_path)
+end
+ 
+# Call the main function
+main
