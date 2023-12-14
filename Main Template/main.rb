bb1e72cbbf00a7ac175b8a8bab5bb2b584496c73
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-12-14 11:45:50 -0300

    FEAT: Adding the Ruby Main Template Directory

---
+++ Main Template/main.rb
@@ -0,0 +1,37 @@
+require "rbconfig" # Import the RbConfig gem
+
+# This method returns the path to the geckodriver executable
+def get_geckodriver_path
+   os = RbConfig::CONFIG["host_os"] # Get the host operating system
+
+   case os # Detect the operating system
+   when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+      # Windows
+      return "path/to/geckodriver.exe"
+   when /darwin|mac os/
+      # macOS
+      return "/path/to/geckodriver"
+   when /linux/
+      # Linux
+      return "/snap/bin/geckodriver" # Specify the path to geckodriver executable
+   else
+      puts "Unsupported operating system: #{os}"
+      return nil
+   end
+end
+
+# Sound file path
+sound_file_path = "./.assets/Sounds/NotificationSound.wav"
+
+# Play sound at the end
+if File.exist?(sound_file_path)
+   if RbConfig::CONFIG["host_os"] =~ /linux/
+      # Linux command to play sound
+      system("aplay #{sound_file_path}")
+   elsif RbConfig::CONFIG["host_os"] =~ /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+      # Windows command to play sound
+      system("start #{sound_file_path}")
+   end
+else
+   puts "Sound file not found: #{sound_file_path}"
+end
